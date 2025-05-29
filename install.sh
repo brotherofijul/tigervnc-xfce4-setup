1fa71f84ae82c7796bea3be9f53e1d2f2c096052
brotherofijul <brotherofijul@gmail.com>
2025-05-29 12:52:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-if [ "$UID" -eq 0 ]; then
-    apt update && sudo apt upgrade -y
-    apt install sudo -y
-else
-    sudo apt update && sudo apt upgrade -y
-fi
-
-sudo apt install dbus-x11 tigervnc-standalone-server xfce4 xfce4-goodies -y
-
+apt update && sudo apt upgrade -y
+apt install dbus-x11 tigervnc-standalone-server xfce4 xfce4-goodies -y
 mkdir -p ~/.vnc
-
 cat <<EOL > ~/.vnc/xstartup
 #!/bin/sh
 
@@ -19,16 +11,6 @@ unset DBUS_SESSION_BUS_ADDRESS
 
 dbus-launch --exit-with-session startxfce4
 EOL
-
 chmod +x ~/.vnc/xstartup
-
-echo "Ketik perintah:"
-echo ""
+echo "# Ketik perintah:"
 echo " vncserver"
-echo "Memulai sesi"
-echo " vncserver :1"
-echo "Memulai sesi ke-1"
-echo " vncserver -list"
-echo "Mengecek sesi"
-echo " vncserver -kill :1"
-echo "menonaktifkan sesi ke-1"
