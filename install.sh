1a791f1eebbb09ffab410fb044c6e03a7b770dbb
brotherofijul <brotherofijul@gmail.com>
2025-05-29 11:11:11 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$UID" -eq 0 ]; then
+    apt install sudo
+fi
+  
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install dbus-x11 tigervnc-standalone-server xfce4 xfce4-goodies -y
+
+mkdir -p ~/.vnc
+
+cat <<EOL > ~/.vnc/xstartup
+#!/bin/sh
+
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+
+dbus-launch --exit-with-session startxfce4
+EOL
+
+sudo chmod +x ~/.vnc/xstartup
+
+vncserver
+
+vncserver -list
